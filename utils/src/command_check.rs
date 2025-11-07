419acfa927bef5badd0f53873de233bccefe2701
Maxi Myoga <l.kupsch1407.new+github@gmail.com>
2025-11-07 17:02:59 +0100

    Added check for CommandRules.channels

--- utils/src/command_check.rs
+++ utils/src/command_check.rs
@@ -1,5 +1,8 @@
 use crate::shared_types::Context;
-use poise::{CreateReply, serenity_prelude::{CacheHttp, GuildId, RoleId}};
+use poise::{
+    CreateReply,
+    serenity_prelude::{CacheHttp, GuildId, RoleId},
+};
 
 /// Checks if the command invoker has at least one of the required roles.
 ///
@@ -29,13 +32,27 @@ pub async fn check(
         }
     }
 
+    if command_rules.channels.is_some() {
+        // Safe to unwrap since we checked for Some
+        if !check_channels(
+            command_rules.channels.unwrap(),
+            command_rules.channel_whitelist,
+            &ctx,
+        )
+        .await
+        {
+            return false;
+        }
+    }
+
     true
 }
 
 async fn check_roles(roles: Vec<RoleId>, ctx: &Context<'_>, guild_id: GuildId) -> bool {
     let mut has_role = false;
     for role in &roles {
-        let result = ctx.author()
+        let result = ctx
+            .author()
             .has_role(ctx.http(), guild_id, role)
             .await
             .unwrap_or(false);
@@ -55,4 +72,21 @@ async fn check_roles(roles: Vec<RoleId>, ctx: &Context<'_>, guild_id: GuildId) -
     }
 
     has_role
+}
+
+async fn check_channels(
+    channels: Vec<poise::serenity_prelude::ChannelId>,
+    channel_whitelist: bool,
+    ctx: &Context<'_>,
+) -> bool {
+    let channel_id = ctx.channel_id();
+    let in_list = channels.contains(&channel_id);
+    let allowed = if channel_whitelist { in_list } else { !in_list };
+    if !allowed {
+        let reply = CreateReply::default()
+            .content(":x: You cannot use this command in this channel!")
+            .ephemeral(true);
+        ctx.send(reply).await.ok();
+    }
+    allowed
 }
